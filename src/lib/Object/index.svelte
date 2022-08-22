3aa0ab6dda40647df4f578e071b39f70c2598161
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-08-23 00:04:24 +0200

    :construction: base

---
+++ src/lib/Object/index.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as THREE from 'three';
+
+	export let scene = {};
+	export let camera = {};
+	export let renderer = {};
+
+	onMount(() => {
+		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		const cube = new THREE.Mesh(geometry, material);
+		scene.add(cube);
+
+		camera.position.z = 5;
+
+		function animate() {
+			requestAnimationFrame(animate);
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+		}
+		animate();
+	});
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import Plane from '$lib/Plane/index.svelte';
+	import * as THREE from 'three';
+	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+	let scene = {};
+	let camera = {};
+	let renderer = {};
+
+	const loadThreeJs = async () => {
+		onMount(() => {
+			const buildScene = () => {
+				const scene = new THREE.Scene();
+				const camera = new THREE.PerspectiveCamera(
+					75,
+					window.innerWidth / window.innerHeight,
+					0.1,
+					1000
+				);
+
+				const renderer = new THREE.WebGLRenderer();
+				renderer.setSize(window.innerWidth, window.innerHeight);
+				document.body.appendChild(renderer.domElement);
+
+				return { scene, camera, renderer };
+			};
+
+			let res = buildScene();
+			scene = res.scene;
+			camera = res.camera;
+			renderer = res.renderer;
+
+			const controls = new OrbitControls(camera, renderer.domElement);
+			camera.position.set(0, 20, 100);
+			controls.update();
+
+			function animate() {
+				requestAnimationFrame(animate);
+				controls.update();
+				renderer.render(scene, camera);
+			}
+			animate();
+		});
+	};
+</script>
+
+{#await loadThreeJs() then _}
+	<Plane bind:scene bind:camera bind:renderer width={100} height={100} />
+{/await}
