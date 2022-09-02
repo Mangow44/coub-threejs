8be5c46c9b9058057f72508b114bddfc8ead2645
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-09-02 18:29:39 +0200

    :construction: Player's camera => cube's camera

--- src/lib/Utils/Player/drawPlayer.js
+++ src/lib/Utils/Player/drawPlayer.js
@@ -3,4 +3,5 @@ export const drawPlayer = (player, controls) => {
 	player.position.x = controls.getObject().position.x;
 	player.position.z = controls.getObject().position.z;
 	player.position.y = controls.getObject().position.y;
+	player.setRotationFromEuler(controls.getObject().rotation);
 };
