bd4913b4960930435676071893deabf91f7ce480
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-09-06 10:00:35 +0200

    :zap: New controls = sprint

--- src/lib/Utils/Player/basicControls.js
+++ src/lib/Utils/Player/basicControls.js
@@ -1,29 +1,37 @@
 export const bindKeyboard = (keyboard) => {
+
+	// To lower case, car lors du sprint, on appuie sur SHIFT qui met les touches en MAJ
+
 	window.addEventListener('keydown', (e) => {
-		keyboard[e.key] = true;
+		let key = e.key.toLowerCase();
+		keyboard[key] = true;
 	});
 
 	window.addEventListener('keyup', (e) => {
-		keyboard[e.key] = false;
+		let key = e.key.toLowerCase();
+		keyboard[key] = false;
 	});
 };
 
 export const processKeyboard = (keyboard, controls, delta = 0) => {
 	if (!controls.getObject) return;
 
-	let speed = 5;
+	let speed = 8;
 	let currentSpeed = speed * delta;
 
-	if (keyboard['w'] || keyboard['z']) {
+	if (keyboard['shift'] == true) currentSpeed = currentSpeed * 2.5;
+
+	if (keyboard['w'] || keyboard['z'] == true) {
 		controls.moveForward(currentSpeed);
 	}
-	if (keyboard['s']) {
+	if (keyboard['s'] == true) {
 		controls.moveForward(-currentSpeed);
 	}
-	if (keyboard['a'] || keyboard['q']) {
+	if (keyboard['a'] || keyboard['q'] == true) {
 		controls.moveRight(-currentSpeed);
 	}
-	if (keyboard['d']) {
+	if (keyboard['d'] == true) {
 		controls.moveRight(currentSpeed);
 	}
 };
+
